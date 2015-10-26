acf8a9cc7d73dce4c06d7b8dde2787da2cc7a8db
Jake Runzer <jakerunzer@gmail.com>
2015-10-25 18:48:59 -0700

    fix port and songs

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-var express     = require('express');
+var express = require('express');
 var serveStatic = require('serve-static');
-var app         = express();
+var app = express();
 
-var PORT        = 7000;
+var PORT = 8000;
 
 app.use(serveStatic(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.sendFile('public/index.html', {root: __dirname})
+app.get('/', function (req, res) {
+  res.sendFile('public/index.html', {
+    root: __dirname
+  })
 });
 
-var server = app.listen(PORT, function() {
+var server = app.listen(PORT, function () {
   var port = server.address().port;
 
   console.log('magic happens on port ' + port);
